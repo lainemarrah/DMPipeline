a824cbe211a2cc6ab0ad717bc886161f555cb2c2
Laine Marrah <87496065+lainemarrah@users.noreply.github.com>
2023-07-10 14:54:28 -0400

    Update loop.sh

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 infile=$1
-while read -r field1 field2; do sbatch --error ${field2}.err --output ${field2}.out --job-name=${field2} pipeline.sh  ${field2} ${field1}; done < ${infile}
+
+outdir=''
+threads=0
+
+while getopts ":o:p:" option; do
+   case "$option" in
+      o)
+  	     outdir="$OPTARG";;
+      t)
+      	 threads="$OPTARG";;
+   esac
+done
+
+while read -r field1 field2 field3; do sbatch --error ${field3}.err --output ${field3}.out --job-name=${field3} pipeline.sh -f1 ${field1} -f2 ${field2} -n ${field3} -o ${outdir} -p ${threads}; done < ${infile}
 #echo "Process complete"
