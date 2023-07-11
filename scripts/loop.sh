62142ecf1c4327377882fce3fe12747b72bfca42
Laine Marrah <87496065+lainemarrah@users.noreply.github.com>
2023-07-11 14:53:52 -0400

    Update loop.sh
    adding chromosome stuff - haven't tested

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -4,15 +4,18 @@ infile=$1
 
 outdir=''
 threads=0
+chr=""
 
 while getopts ":o:p:" option; do
    case "$option" in
       o)
   	     outdir="$OPTARG";;
       t)
-      	 threads="$OPTARG";;
+        threads="$OPTARG";;
+      c) 
+        chr="$OPTARG";;
    esac
 done
 
-while read -r field1 field2 field3; do sbatch --error ${field3}.err --output ${field3}.out --job-name=${field3} pipeline.sh -f1 ${field1} -f2 ${field2} -n ${field3} -o ${outdir} -p ${threads}; done < ${infile}
+while read -r field1 field2 field3; do sbatch --error ${field3}.err --output ${field3}.out --job-name=${field3} pipeline.sh -f1 ${field1} -f2 ${field2} -n ${field3} -o ${outdir} -p ${threads} -c ${chr}; done < ${infile}
 #echo "Process complete"
