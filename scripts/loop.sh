83e97475684e6f81c71f5f4e19429a1040430cd6
Laine Marrah <87496065+lainemarrah@users.noreply.github.com>
2023-08-04 10:47:22 -0400

    Update loop.sh
    fixing issues with variables

--- scripts/loop.sh
+++ scripts/loop.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-infile=$1
-
+infile=''
 outdir=''
 threads=0
-chr=""
+chr=''
 
-while getopts ":o:p:" option; do
+while getopts ":i:o:p:c:" option; do
    case "$option" in
+      i)
+        infile="$OPTARG";;
       o)
   	     outdir="$OPTARG";;
       p)
         threads="$OPTARG";;
       c) 
         chr="$OPTARG";;
+     \?)
+        echo "Invalid option"
+        exit;;
    esac
 done
 
-while read -r field1 field2 field3; do mkdir ${outdir}/${field3}; sbatch --error ${field3}.err --output ${field3}.out --job-name=${field3} pipeline.sh -1 ${field1} -2 ${field2} -n ${field3} -o ${outdir}/${field3} -p ${threads} -c ${chr}; done < ${infile}
+while read -r field1 field2 field3; do sbatch --error ${field3}.err --output ${field3}.out --job-name=${field3} scripts/pipeline.sh -1 ${field1} -2 ${field2} -n ${field3} -o ${outdir}/${field3} -p ${threads} -c ${chr}; done < ${infile}
 #echo "Process complete"
